cb00caa48efc6066044cc2d29f1ce0ecb97b9b1c
Junhui Yang <yangjunhuiyjh@gmail.com>
2020-01-18 21:03:44 +0000

    Change bot specs

--- Game/Level.js
+++ Game/Level.js
@@ -3,22 +3,22 @@ import {Vec2} from "../Game/Vec2.js"
 
 const getGameOfLevel = (level) => {
     if (level === 0) {
-        let game = new Game(new Vec2(2,2), new Vec2(2,1));
-        game.bots.push(new Bot(new Vec2(0,1), new Vec2(0,1),5));
+        let game = new Game(new Vec2(2,2), new Vec2(1,2));
+        game.bots.push(new Bot(new Vec2(1,0), new Vec2(0,1),5));
         return game
     }
     if (level === 1) {
         let game = new Game(new Vec2(3,3), new Vec2(3,0));
         game.bots.push(new Bot(new Vec2(0,0), new Vec2(0,1),5));
-        game.hackers.push(new Hacker(new Vec2(1,2 ), new Vec2(-1, 0)))
-        game.hackers.push(new Hacker(new Vec2(3,1 ), new Vec2(1, 0)))
+        game.hackers.push(new Hacker(new Vec2(2,1 ), new Vec2(-1, 0)))
+        game.hackers.push(new Hacker(new Vec2(1,3 ) new Vec2(1, 0)))
         return game
     }
     if (level === 2) {
         let game = new Game(new Vec2(5,5), new Vec2(5,5));
         game.bots.push(new Bot(new Vec2(0,0), new Vec2(0,1),5));
-        game.hackers.push(new Hacker(new Vec2(3,1 ), new Vec2(0, -1)))
-        game.hackers.push(new Hacker(new Vec2(4,3 ), new Vec2(-1, 0)))
+        game.hackers.push(new Hacker(new Vec2(1,3 ), new Vec2(0, -1)))
+        game.hackers.push(new Hacker(new Vec2(3,4 ), new Vec2(-1, 0)))
         game.hackers.push(new Hacker(new Vec2(2,2 ), new Vec2(1, 0)))
         return game
     }
