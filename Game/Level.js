cb8f0639a611091966de7961fb328055291b3e0b
Junhui Yang <yangjunhuiyjh@gmail.com>
2020-01-19 05:36:29 +0000

    Add games to teach for loop, while loop, array

--- Game/Level.js
+++ Game/Level.js
@@ -1,6 +1,6 @@
 import {Game,Messenger,Hacker} from "../Game/Game.js"
 import {Vec2} from "../Game/Vec2.js"
-import {MODE_ALL_MOVES, MODE_NEXT_MOVE} from "../Game/Game";
+import {MODE_ALL_MOVES, MODE_NEXT_MOVE} from "../Game/Game.js";
 
 const getGameOfLevel = (level) => {
     if (level === 0) {
@@ -9,6 +9,16 @@ const getGameOfLevel = (level) => {
         return game
     }
     if (level === 1) {
+        let game = new Game(new Vec2(10,1), new Vec2(10,0), MODE_ALL_MOVES,5);
+        game.messengers.push(new Messenger(new Vec2(0,0), new Vec2(0,1),5));
+        return game
+    }
+    if (level === 2) {
+        let game = new Game(new Vec2(1,10), new Vec2(0,10), MODE_ALL_MOVES,5);
+        game.messengers.push(new Messenger(new Vec2(0,0), new Vec2(0,1),5));
+        return game
+    }
+    if (level === 3) {
         let game = new Game(new Vec2(4, 4), new Vec2(2, 4), MODE_ALL_MOVES, 5);
         game.messengers.push(new Messenger(new Vec2(2, 0), new Vec2(0, 1), 5));
         let hacker = new Hacker(new Vec2(2, 2), new Vec2(-1, 0))
@@ -24,12 +34,25 @@ const getGameOfLevel = (level) => {
         game.hackers.push(hacker)
         return game
     }
-    if (level === 2) {
-        let game = new Game(new Vec2(5,5), new Vec2(5,5), MODE_ALL_MOVES, 5);
-        game.messengers.push(new Messenger(new Vec2(0,0), new Vec2(0,1),5));
-        game.hackers.push(new Hacker(new Vec2(1,3 ), new Vec2(0, -1)))
-        game.hackers.push(new Hacker(new Vec2(3,4 ), new Vec2(-1, 0)))
-        game.hackers.push(new Hacker(new Vec2(2,2 ), new Vec2(1, 0)))
+    if (level === 4) {
+        let game = new Game(new Vec2(10, 10), new Vec2(10, 10), MODE_ALL_MOVES, 5);
+        game.messengers.push(new Messenger(new Vec2(0, 0), new Vec2(0, 1), 5));
+        game.hackers.push(new Hacker(new Vec2(1, 1), new Vec2(-1, 0)))
+        game.hackers.push(new Hacker(new Vec2(3, 3), new Vec2(-1, 0)))
+        game.hackers.push(new Hacker(new Vec2(5, 5), new Vec2(-1, 0)))
+        game.hackers.push(new Hacker(new Vec2(7, 7), new Vec2(-1, 0)))
+        game.hackers.push(new Hacker(new Vec2(9, 9), new Vec2(-1, 0)))
+        for (let hacker of game.hackers) {
+            hacker.getNextMove = (state) => {
+                if (hacker.position.x === 0 && hacker.direction.x === -1) {
+                    return {x: 1, y: 0, goInvisible: false}
+                } else if (hacker.position.x === 4 && hacker.direction.x === 1) {
+                    return {x: -1, y: 0, goInvisible: false}
+                } else {
+                    return {x: hacker.direction.x, y: 0, goInvisible: false}
+                }
+            }
+        }
         return game
     }
 }
